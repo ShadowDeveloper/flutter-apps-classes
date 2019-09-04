c7986efc2bd47d1a15eda13e465b112adb2fd610
ShadowDeveloper <ro_web1@hotmail.com>
2019-09-04 06:44:34 -0300

    Ajustes no conversor de moeda

--- conversor_de_moeda/lib/main.dart
+++ conversor_de_moeda/lib/main.dart
@@ -1,20 +1,92 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
-const request = "https://api.hgbrasil.com/finance?format=json-cors&key=570d905d";
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=570d905d";
 
 void main() async {
-
-  http.Response response = await http.get(request);
-
-
-  print(json.decode(response.body));
+  print(await getData());
 
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
 }
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao carregar Dados",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                 child: Column(
+                   crossAxisAlignment: CrossAxisAlignment.stretch,
+                   children: <Widget>[
+                     Icon(
+                         Icons.monetization_on,
+                         size: 150.0, color: Colors.amber
+                     )
+                   ],
+                 ),
+                );
+
+              }
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
