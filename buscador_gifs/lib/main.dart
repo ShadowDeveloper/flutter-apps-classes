a7fe4fd0d581450977c7c51dc439f382a59fc875
Rodrigo Souza <rodrigo.souza@norber.com.br>
2019-10-08 17:27:02 -0300

    Carregando grid de gifs e realizando busca

--- buscador_gifs/lib/main.dart
+++ buscador_gifs/lib/main.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:share/share.dart';
-import 'package:http/http.dart' as http;
+
 import 'package:transparent_image/transparent_image.dart';
 
+import 'ui/home_page.dart';
 
 
 void main(){
   runApp(MaterialApp(
-    home: Home()
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)
+          )
+      )
+    ),
   ));
-}
-
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Icon(Icons.sort, color: Colors.white,)
-          ],
-        ),
-      ),
-    );
-  }
 }
