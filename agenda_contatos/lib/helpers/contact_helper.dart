7ff11df611fa04d369b8048fbb96b9f2e9f616eb
ShadowDeveloper <ro_web1@hotmail.com>
2019-12-16 21:53:46 -0300

    Dialog para quando voltar sem salvar o contato

--- agenda_contatos/lib/helpers/contact_helper.dart
+++ agenda_contatos/lib/helpers/contact_helper.dart
@@ -1,27 +1,28 @@
-import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 final String contactTable = "contactTable";
-
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
-final String phoneColumn = "phoneColumn";
 final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper {
 
-  //#region Define a Classe como Singleton, ou seja, toda instancia desta classe irá apontar para uma única chamada da mesma e não criará objetos diferentes em cada chamada
   static final ContactHelper _instance = ContactHelper.internal();
+
   factory ContactHelper() => _instance;
+
   ContactHelper.internal();
-  //#endregion
 
   Database _db;
 
   Future<Database> get db async {
-    if (_db != null) {
-      return db;
+    if(_db != null){
+      return _db;
     } else {
       _db = await initDb();
       return _db;
@@ -29,13 +30,14 @@ class ContactHelper {
   }
 
   Future<Database> initDb() async {
-    final dataBasesPath = await getDatabasesPath();
-    final path = join(dataBasesPath, "contacts.db");
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contactsnew.db");
 
-    return await openDatabase(path, version: 1,
-        onCreate: (Database db, int newerVersion) async {
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
       await db.execute(
-          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $phoneColumn TEXT, $emailColumn TEXT, $imgColumn TEXT)");
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT,"
+            "$phoneColumn TEXT, $imgColumn TEXT)"
+      );
     });
   }
 
@@ -48,11 +50,10 @@ class ContactHelper {
   Future<Contact> getContact(int id) async {
     Database dbContact = await db;
     List<Map> maps = await dbContact.query(contactTable,
-        columns: [idColumn, nameColumn, phoneColumn, emailColumn, imgColumn],
-        where: "$idColumn = ?",
-        whereArgs: [id]);
-
-    if (maps.length > 0) {
+      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+      where: "$idColumn = ?",
+      whereArgs: [id]);
+    if(maps.length > 0){
       return Contact.fromMap(maps.first);
     } else {
       return null;
@@ -61,26 +62,24 @@ class ContactHelper {
 
   Future<int> deleteContact(int id) async {
     Database dbContact = await db;
-    return await dbContact
-        .delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
   }
 
   Future<int> updateContact(Contact contact) async {
     Database dbContact = await db;
-    return await dbContact.update(contactTable, contact.toMap(),
-        where: "$idColumn = ?", whereArgs: [contact.id]);
+    return await dbContact.update(contactTable,
+        contact.toMap(),
+        where: "$idColumn = ?",
+        whereArgs: [contact.id]);
   }
 
-  Future<List<Contact>> getAllContacts() async {
+  Future<List> getAllContacts() async {
     Database dbContact = await db;
     List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
-
-    List<Contact> listContact = new List(); // Ou somente List();
-
-    for (Map m in listMap) {
+    List<Contact> listContact = List();
+    for(Map m in listMap){
       listContact.add(Contact.fromMap(m));
     }
-
     return listContact;
   }
 
@@ -89,7 +88,7 @@ class ContactHelper {
     return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
   }
 
-  Future close() async{
+  Future close() async {
     Database dbContact = await db;
     dbContact.close();
   }
@@ -98,41 +97,38 @@ class ContactHelper {
 
 class Contact {
 
-  Contact();
-
   int id;
   String name;
-  String phone;
   String email;
+  String phone;
   String img;
 
-  Contact.fromMap(Map map) {
+  Contact();
+
+  Contact.fromMap(Map map){
     id = map[idColumn];
     name = map[nameColumn];
-    phone = map[phoneColumn];
     email = map[emailColumn];
+    phone = map[phoneColumn];
     img = map[imgColumn];
-  } //Isso serve para quando o Map com os dados do contato for passado ele irá
-  // definir os campos iguais ao da coluna do banco.
-  // De um mapa(Json) e passou para o contato
+  }
 
   Map toMap() {
     Map<String, dynamic> map = {
       nameColumn: name,
-      phoneColumn: phone,
       emailColumn: email,
-      imgColumn: img,
+      phoneColumn: phone,
+      imgColumn: img
     };
-
-    if (id != null) {
+    if(id != null){
       map[idColumn] = id;
     }
-
     return map;
   }
 
   @override
   String toString() {
-    return "Contact(id: $id, name: $name, phone: $phone, email: $email, img: $img)";
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
   }
+
 }
--- agenda_contatos/lib/ui/contact_page.dart
+++ agenda_contatos/lib/ui/contact_page.dart
@@ -37,76 +37,111 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.white),
-        elevation: 0.0,
-        title: Text(_editedContact.name ?? "Novo contato"),
-        textTheme: TextTheme(),
-        centerTitle: false,
-        backgroundColor: Colors.deepOrangeAccent,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: 140.0,
-                height: 140.0,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: _editedContact.img != null &&
-                                _editedContact.img.isNotEmpty
-                            ? FileImage(File(_editedContact.img))
-                            : AssetImage("images/user.png"),
-                        repeat: ImageRepeat.noRepeat)),
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white),
+          elevation: 0.0,
+          title: Text(_editedContact.name ?? "Novo contato"),
+          textTheme: TextTheme(),
+          centerTitle: false,
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  width: 140.0,
+                  height: 140.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: _editedContact.img != null &&
+                                  _editedContact.img.isNotEmpty
+                              ? FileImage(File(_editedContact.img))
+                              : AssetImage("images/user.png"),
+                          repeat: ImageRepeat.noRepeat)),
+                ),
               ),
-            ),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: "Nome"),
-              focusNode: _nameFocus,
-              onChanged: (String text) {
-                _userEdited = true;
-                setState(() {
-                  _editedContact.name = text;
-                });
-              },
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "Email"),
-              onChanged: (String text) {
-                _userEdited = true;
-                _editedContact.email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _phoneController,
-              decoration: InputDecoration(labelText: "Phone"),
-              onChanged: (String text) {
-                _userEdited = true;
-                _editedContact.phone = text;
-              },
-              keyboardType: TextInputType.phone,
-            ),
-          ],
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: "Nome"),
+                focusNode: _nameFocus,
+                onChanged: (String text) {
+                  _userEdited = true;
+                  setState(() {
+                    _editedContact.name = text;
+                  });
+                },
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: "Email"),
+                onChanged: (String text) {
+                  _userEdited = true;
+                  _editedContact.email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: "Phone"),
+                onChanged: (String text) {
+                  _userEdited = true;
+                  _editedContact.phone = text;
+                },
+                keyboardType: TextInputType.phone,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+              Navigator.pop(context, _editedContact);
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+          backgroundColor: Colors.deepOrange,
+          child: Icon(Icons.save, color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
-            Navigator.pop(context, _editedContact);
-          } else {
-            FocusScope.of(context).requestFocus(_nameFocus);
-          }
-        },
-        backgroundColor: Colors.deepOrange,
-        child: Icon(Icons.save, color: Colors.white),
-      ),
     );
+  }
+
+  Future<bool> _requestPop() async {
+    if (_userEdited) {
+      showDialog(
+          barrierDismissible: true,
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Descartar alterações?"),
+              content: Text("Se sair as alterações serão perdidas"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Cancelar"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                FlatButton(
+                  child: Text("Sim"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            );
+          });
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
 }
